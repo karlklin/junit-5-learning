ff87d4c6e17d7ed9fb3afcf4262551352a67c86b
Karol Pawelski <karol.pawelski@cognifide.com>
2017-12-11 14:49:52 +0100

    defining profiles and splitting test suites

--- src/test/java/com/github/karlklin/biz/MyBusinessLogicTest.java
+++ src/test/java/com/github/karlklin/biz/MyBusinessLogicTest.java
@@ -5,6 +5,7 @@ import org.junit.jupiter.api.*;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertThrows;
 
+@Tag("acceptance")
 public class MyBusinessLogicTest {
 
     private MyBusinessLogic businessLogic;
