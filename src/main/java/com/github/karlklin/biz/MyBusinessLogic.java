a4d19d74884e477fcd130508c970ab5258fe60a5
Karol Pawelski <karol.pawelski@cognifide.com>
2017-12-11 12:08:32 +0100

    splitting into test cases using @RunWith and @SelectedPackages() for JUnit 4 support.

---
+++ src/main/java/com/github/karlklin/biz/MyBusinessLogic.java
@@ -0,0 +1,9 @@
+package com.github.karlklin.biz;
+
+public class MyBusinessLogic {
+
+    public void businessMethodThatThrowsException(){
+        throw new UnsupportedOperationException("Not supported");
+    }
+
+}
